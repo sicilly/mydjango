11cb4d1b1f4c8a8295d29b5721d7b1b567ab3526
sicilly <634068240@qq.com>
2020-01-31 16:36:36 +0800

    测试用户的路由urls

--- mydjango/users/tests/test_urls.py
+++ mydjango/users/tests/test_urls.py
@@ -1,24 +1,58 @@
-import pytest
+# import pytest
+# from django.urls import reverse, resolve
+#
+# from mydjango.users.models import User
+#
+# pytestmark = pytest.mark.django_db
+#
+#
+# def test_detail(user: User):
+#     assert (
+#         reverse("users:detail", kwargs={"username": user.username})
+#         == f"/users/{user.username}/"
+#     )
+#     assert resolve(f"/users/{user.username}/").view_name == "users:detail"
+#
+#
+# def test_update():
+#     assert reverse("users:update") == "/users/~update/"
+#     assert resolve("/users/~update/").view_name == "users:update"
+#
+#
+# def test_redirect():
+#     assert reverse("users:redirect") == "/users/~redirect/"
+#     assert resolve("/users/~redirect/").view_name == "users:redirect"
 from django.urls import reverse, resolve
-
-from mydjango.users.models import User
-
-pytestmark = pytest.mark.django_db
+from test_plus import TestCase
 
 
-def test_detail(user: User):
-    assert (
-        reverse("users:detail", kwargs={"username": user.username})
-        == f"/users/{user.username}/"
-    )
-    assert resolve(f"/users/{user.username}/").view_name == "users:detail"
+class TestUserUrls(TestCase):
+    def setUp(self):  # 创建用户
+        self.user = self.make_user(username='testuser', password='password')
 
+    # 详情页
+    def test_detail_reverse(self):  # 反向解析测试 路由名称->路径字符串
+        reversed_url = reverse("users:detail", kwargs={"username": self.user.username})
+        self.assertEqual(reversed_url, f"/users/{self.user.username}/")
 
-def test_update():
-    assert reverse("users:update") == "/users/~update/"
-    assert resolve("/users/~update/").view_name == "users:update"
+    def test_detail_resolve(self):  # 正向解析测试  路径字符串->路由名称
+        resolved_urlname = resolve(f"/users/{self.user.username}/").view_name
+        self.assertEqual(resolved_urlname, "users:detail")
 
+    # 更新
+    def test_update_reverse(self):  # 反向解析测试 路由名称->路径字符串
+        reversed_url = reverse("users:update")
+        self.assertEqual(reversed_url, "/users/~update/")
 
-def test_redirect():
-    assert reverse("users:redirect") == "/users/~redirect/"
-    assert resolve("/users/~redirect/").view_name == "users:redirect"
+    def test_update_resolve(self):  # 正向解析测试  路径字符串->路由名称
+        resolved_urlname = resolve("/users/~update/").view_name
+        self.assertEqual(resolved_urlname, "users:update")
+
+    # 重定向
+    def test_redirect_reverse(self):  # 反向解析测试 路由名称->路径字符串
+        reversed_url = reverse("users:redirect")
+        self.assertEqual(reversed_url, "/users/~redirect/")
+
+    def test_redirect_resolve(self):  # 正向解析测试  路径字符串->路由名称
+        resolved_urlname = resolve("/users/~redirect/").view_name
+        self.assertEqual(resolved_urlname, "users:redirect")
--- mydjango/users/urls.py
+++ mydjango/users/urls.py
@@ -8,9 +8,9 @@ from mydjango.users.views import (
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
+    path("~redirect/", view=user_redirect_view, name="redirect"),  # 重定向
+    path("~update/", view=user_update_view, name="update"),  # 更新
     # 传用户名作为参数到详情页 这里的username和模型中的字段无关，应该和views.py中的slug_url_kwarg一致
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    path("<str:username>/", view=user_detail_view, name="detail"),  # 用户详情页
     # path("<int:pk>", view=user_detail_view, name="detail"),  # pk的写法
 ]
