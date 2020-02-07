b863245b568f0a65cefe39dcfb737a6975498a1a
sicilly <634068240@qq.com>
2020-02-07 16:40:40 +0800

    答案类 Answer 模型的设计

--- mydjango/quora/models.py
+++ mydjango/quora/models.py
@@ -3,10 +3,6 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 
-class Answer(models.Model):
-    pass
-
-
 class QuestionQuerySet(models.query.QuerySet):
     """自定义QuerySet，提高模型类的可用性"""
 
@@ -65,3 +61,23 @@ class Question(models.Model):
         return Answer.objects.get(question=self, is_accepted=True)
 
 
+class Answer(models.Model):
+    """问题答案"""
+    # uuid_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="answers",
+                             on_delete=models.CASCADE, verbose_name='回答者')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers", verbose_name='问题')
+    content = models.TextField(verbose_name='答案内容')
+    is_accepted = models.BooleanField(default=False, verbose_name='是否被接受')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+
+    # votes = GenericRelation(Vote, verbose_name='投票情况')  # 通过GenericRelation关联到Vote表
+
+    class Meta:
+        ordering = ('-is_accepted', '-created_at')  # 多字段排序
+        verbose_name = '答案'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.content
