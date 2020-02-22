81179b046c8f83cb7dec875e5e236c70dba297d2
sicilly <634068240@qq.com>
2020-02-22 22:36:10 +0800

    实现通知处理器函数

--- mydjango/notifications/views.py
+++ mydjango/notifications/views.py
@@ -1,3 +1,5 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -52,3 +54,34 @@ def mark_as_read(request, slug):
         return redirect(redirect_url)
 
     return redirect('notifications:unread')
+
+
+def notification_headler(actor, recipient, verb, action_object, **kwargs):
+    """
+    通知处理器
+    :param actor:           request.user对象
+    :param recipient:       User Instance 接收者实例，可以是一个或者多个接收者
+    :param verb:            str通知类别
+    :param action_object:   Instance 动作对象的实例
+    :param kwargs           key, id_value等
+    :return:                None
+    """
+    if actor.username != recipient.username and recipient.username == action_object.user.username:
+        # 只通知接收者，即recipient == 动作对象的作者
+        key = kwargs.get('key', 'notification')
+        id_value = kwargs.get('id_value', None)
+        # 记录通知内容
+        Notification.objects.create(
+            actor=actor,
+            recipient=recipient,
+            verb=verb,
+            action_object=action_object
+        )
+        payload = {
+            'type': 'receive',
+            'key': key,
+            'actor_name': actor.username,
+            'id_value': id_value
+        }
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)('notifications', payload)
